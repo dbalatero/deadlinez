ed9a04d66176986b1f0ca21953dbf129f849fb74
David Balatero <dbalatero@gmail.com>
2010-07-22 09:21:04 -0700

    Send headers for all request types.

--- lib/deadlinez/api.rb
+++ lib/deadlinez/api.rb
@@ -4,6 +4,12 @@ module Deadlinez
     format :xml
     base_uri 'service.afterthedeadline.com'
 
+    REQUIRED_HEADERS = {
+      'Host' => 'service.afterthedeadline.com',
+      'Content-Type' => 'application/x-www-form-urlencoded',
+      'User-Agent' => 'AtD/0.1'
+    }
+
     # Proxies any URL through to AtD. Useful for the TinyMCE
     # plugin.
     def self.proxy(path, params)
@@ -11,11 +17,7 @@ module Deadlinez
       response = post(path,
                       :body => proxy_params,
                       :format => nil,
-                      :headers => {
-                        'Host' => 'service.afterthedeadline.com',
-                        'Content-Type' => 'application/x-www-form-urlencoded',
-                        'User-Agent' => 'AtD/0.1'
-                      })
+                      :headers => REQUIRED_HEADERS)
       response.body
     end
 
@@ -23,7 +25,8 @@ module Deadlinez
       response = post('/stats',
                      :body => {
                        :data => data
-                     })
+                     },
+                     :headers => REQUIRED_HEADERS)
       response['scores']['metric']
     end
   end
